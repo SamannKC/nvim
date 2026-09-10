a29d675ea9439b75a220c4626d9347ec23f24d3e
SamannKC <np03cs4a250131@heraldcollege.edu.np>
2026-09-11 03:32:02 +0545

    removed bindings for git signs

--- lua/zukie/packer.lua
+++ lua/zukie/packer.lua
@@ -27,9 +27,6 @@ return require('packer').startup(function(use)
 
   use {
 	  "lewis6991/gitsigns.nvim",
-	  config = function()
-		  require("gitsigns").setup()
-	  end,
   }
 
   use {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,7 +75,6 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["gitsigns.nvim"] = {
-    config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0" },
     loaded = true,
     path = "/home/ado/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
@@ -138,10 +137,6 @@ time([[Config for toggleterm.nvim]], false)
 time([[Config for neovim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "neovim")
 time([[Config for neovim]], false)
--- Config for: gitsigns.nvim
-time([[Config for gitsigns.nvim]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
-time([[Config for gitsigns.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
